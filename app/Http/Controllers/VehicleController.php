8ef5d2560cbea1ba110d84592878d8f73246856e
Rafael Souza <rafa.nds13@gmail.com>
2025-08-25 14:43:12 -0300

    Criando o controlador para vehicle e implementando o método index com paginação e show

---
+++ app/Http/Controllers/VehicleController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use \Illuminate\Database\Eloquent\ModelNotFoundException;
+class VehicleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $vehicles = Vehicle::paginate(5);
+        return response()->json($vehicles, 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try{
+            $vehicle = Vehicle::findOrFail($id);
+        }catch(ModelNotFoundException $e){
+            return response()->json(['error' => 'Vehicle not found'], 404);
+        }
+        return response()->json($vehicle, 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
