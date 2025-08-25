70bf60c299d00d474e8cc990eae300ab4f4516c1
Rafael Souza <rafa.nds13@gmail.com>
2025-08-25 14:02:52 -0300

    Atualizando o form request de Vehicle

--- app/Http/Requests/VehicleRequest.php
+++ app/Http/Requests/VehicleRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Vehicle;
 
 class VehicleRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class VehicleRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -21,23 +22,23 @@ class VehicleRequest extends FormRequest
      */
     public function rules(){
         return [
-            'json_data_id' => 'required|integer|unique:vehicles,json_data_id',
+            'json_data_id' => 'required|integer',
             'type'         => 'required|string',
             'brand'        => 'required|string',
             'model'        => 'required|string',
             'version'      => 'required|string',
             'doors'        => 'required|string',
             'board'        => 'required|string',
-            'chassi'       => 'required|string',
+            'chassi'       => 'nullable|string',
             'transmission' => 'required|string',
             'km'           => 'required|string',
-            'description'  => 'required|string',
+            'description'  => 'nullable|string',
             'created_json' => 'required|date',
             'updated_json' => 'required|date',
             'sold'         => 'boolean',
             'category'     => 'required|string|max:100',
-            'url_car'      => 'required|url',
-            'old_price'    => 'required|numeric|min:0',
+            'url_car'      => 'required|string',
+            'old_price'    => 'nullable|numeric|min:0',
             'price'        => 'required|numeric|min:0',
             'color'        => 'required|string',
             'fuel'         => 'required|string',
