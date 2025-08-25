b7f8ccc6123f18a840b89d65a87592dfc7cf95ca
Rafael Souza <rafa.nds13@gmail.com>
2025-08-25 19:40:22 -0300

    Seed para criar um usuário autenticado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'usuario',
+            'email' => 'usuario@example.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('senha123'),
+            'remember_token' => Str::random(10),
         ]);
     }
 }
