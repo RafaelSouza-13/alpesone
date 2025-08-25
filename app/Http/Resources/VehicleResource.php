22de78047c02d87e32e175b7357d68a5d72c7fa8
Rafael Souza <rafa.nds13@gmail.com>
2025-08-25 15:18:42 -0300

    Criando um resource para vehicle e retornar os dados que estao no banco de dados

---
+++ app/Http/Resources/VehicleResource.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class VehicleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'json_data_id' => $this->json_data_id,
+            'type'         => $this->type,
+            'brand'        => $this->brand,
+            'model'        => $this->model,
+            'version'      => $this->version,
+            'doors'        => $this->doors,
+            'board'        => $this->board,
+            'chassi'       => $this->chassi,
+            'transmission' => $this->transmission,
+            'km'           => $this->km,
+            'description'  => $this->description,
+            'created'      => $this->created,
+            'updated'      => $this->updated,
+            'sold'         => $this->sold,
+            'category'     => $this->category,
+            'url_car'      => $this->url_car,
+            'old_price'    => $this->old_price,
+            'price'        => $this->price,
+            'color'        => $this->color,
+            'fuel'         => $this->fuel,
+            'year'         => json_decode($this->year, true),
+            'optionals'    => json_decode($this->optionals, true),
+            'fotos'        => json_decode($this->fotos, true),
+        ];
+    }
+}
